702f9d9f51970501a7f632b6a47ed8920dfb2b16
a.merlin <a.merlin@proxiad.com>
2026-07-30 11:23:20 +0200

    move dark mode toggle to Me page as a switch

--- ProxiBetApp/ProxiBetApp/PageModels/HomePageModel.cs
+++ ProxiBetApp/ProxiBetApp/PageModels/HomePageModel.cs
@@ -1,18 +1,13 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using ProxiBetApp.Services.Auth;
-using ProxiBetApp.Services.Theme;
 
 namespace ProxiBetApp.PageModels
 {
     public partial class HomePageModel : PageModelBase
     {
         private readonly IAuthService _authService;
-        private readonly IThemeService _themeService;
         private readonly CurrentUserStore _currentUserStore;
-
-        [ObservableProperty]
-        private bool _isDarkMode;
 
         public AuthenticatedUser? User => _currentUserStore.CurrentUser;
 
@@ -23,10 +18,9 @@ namespace ProxiBetApp.PageModels
             false;
 #endif
 
-        public HomePageModel(IAuthService authService, IThemeService themeService, CurrentUserStore currentUserStore)
+        public HomePageModel(IAuthService authService, CurrentUserStore currentUserStore)
         {
             _authService = authService;
-            _themeService = themeService;
             _currentUserStore = currentUserStore;
 
             _currentUserStore.PropertyChanged += (_, e) =>
@@ -34,8 +28,6 @@ namespace ProxiBetApp.PageModels
                 if (e.PropertyName == nameof(Services.Auth.CurrentUserStore.CurrentUser))
                     OnPropertyChanged(nameof(User));
             };
-
-            IsDarkMode = _themeService.CurrentTheme == AppTheme.Dark;
         }
 
         [RelayCommand]
@@ -51,22 +43,9 @@ namespace ProxiBetApp.PageModels
         }
 
         [RelayCommand]
-        private static async Task GoToProfileAsync()
-        {
-            await Shell.Current.GoToAsync("me");
-        }
-
-        [RelayCommand]
         private static async Task GoToDevGalleryAsync()
         {
             await Shell.Current.GoToAsync("dev/gallery");
-        }
-
-        [RelayCommand]
-        private async Task ToggleThemeAsync()
-        {
-            await _themeService.ToggleThemeAsync();
-            IsDarkMode = _themeService.CurrentTheme == AppTheme.Dark;
         }
     }
 }
